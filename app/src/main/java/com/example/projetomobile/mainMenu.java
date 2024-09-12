6f2931eab131b9dc82c1ba22497404f20787fb7d
justino vinicius <justino.v1996@gmail.com>
2024-09-12 11:25:17 -0300

    new conteudo

--- app/src/main/java/com/example/projetomobile/mainMenu.java
+++ app/src/main/java/com/example/projetomobile/mainMenu.java
@@ -1,6 +1,8 @@
 package com.example.projetomobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class mainMenu extends AppCompatActivity {
 
+    private androidx.appcompat.widget.AppCompatButton text_adicionaproduto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainMenu), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        IniciarComponentes();
+        {
+            text_adicionaproduto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mainMenu.this, add.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        }
+    private void IniciarComponentes() {
+        text_adicionaproduto = findViewById(R.id.text_adicionaproduto);
     }
 }
